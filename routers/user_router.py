fc6185bf2ffa764de7e4e9e0f66135df24b6ba9b
Henry <henryhuang509@gmail.com>
2023-12-28 00:34:03 +0800

    rename and refactor functions

--- routers/user_router.py
+++ routers/user_router.py
@@ -2,13 +2,13 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
-from jose import JWTError
 
 from database import DatabaseDependency
+from models.user import User as DB_User
 from schemas.user_schemas import User, UserCreate, UserUpdate
 from schemas.util_schemas import ExceptionMessage
 from utils.crud import UserCRUD
-from utils.jwt import TokenDependency, decode_token
+from utils.jwt import TokenDependency, get_jwt_username
 
 
 user_router = APIRouter(
@@ -17,29 +17,26 @@ user_router = APIRouter(
 )
 
 async def get_current_user(token: TokenDependency, db: DatabaseDependency) -> User:
-    try:
-        payload = decode_token(token)
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid Credentials")
-    username = payload.get("sub")
+    username = get_jwt_username(token)
     user = await UserCRUD.get_user_by_username(db, username=username)
     if user is None:
         raise HTTPException(status_code=401, detail="Invalid Credentials")
     return user
+JWT_USER_DEPENDENCY = Annotated[DB_User, Depends(get_current_user)]
 
 @user_router.get("/me", responses={401: {"model": ExceptionMessage}})
-async def read(user: Annotated[User, Depends(get_current_user)]) -> User:
+async def get_user(user: JWT_USER_DEPENDENCY) -> User:
     return user
 
 @user_router.post("", status_code= 201, responses={409: {"model": ExceptionMessage}})
-async def create(user: UserCreate, db: DatabaseDependency) -> User:
+async def create_user(user: UserCreate, db: DatabaseDependency) -> User:
     try:
         return await UserCRUD.create_user(db, user)
     except IntegrityError:
         raise HTTPException(status_code=409, detail="Username Already Exists")
 
 @user_router.put("", responses = {401: {"model": ExceptionMessage}, 409: {"model": ExceptionMessage}})
-async def update(updates: UserUpdate, user: Annotated[User, Depends(get_current_user)], db: DatabaseDependency) -> User:
+async def update_user(updates: UserUpdate, user: JWT_USER_DEPENDENCY, db: DatabaseDependency) -> User:
     try:
         result = await UserCRUD.update_user(db, user.id, updates)
     except IntegrityError:
@@ -47,5 +44,5 @@ async def update(updates: UserUpdate, user: Annotated[User, Depends(get_current_
     return result
 
 @user_router.delete("", status_code=204)
-async def delete(user: Annotated[User, Depends(get_current_user)], db: DatabaseDependency) -> None:
+async def delete_user(user: JWT_USER_DEPENDENCY, db: DatabaseDependency) -> None:
     await UserCRUD.delete_user_by_username(db, user.username)
