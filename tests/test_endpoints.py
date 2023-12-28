cec6591a369f3011fd5c43487cb81f3478b0c797
Henry <henryhuang509@gmail.com>
2023-12-28 23:42:41 +0800

    update tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -2,73 +2,37 @@ import pytest
 
 from env_setup import client, db_setup, anyio_backend
 
-class TestEndpoints:
+class TestUserEndpoints:
     @pytest.mark.anyio
     async def test_create_user(self, client, db_setup):
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         response = await client.post("/users", json=user)
         response_data = response.json()
         assert response.status_code == 201
         assert response_data["username"] == "test"
-        assert response_data["email"] == "email"
+        assert response_data["email"] == "email@gmail.com"
     
     @pytest.mark.anyio
     async def test_create_user_already_exist(self, client, db_setup):
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.post("/users", json=user)
         assert response.status_code == 409
     
     @pytest.mark.anyio
-    async def test_token(self, client, db_setup):
-        user = {
-            "username": "test",
-            "password": "password",
-            "email": "email"
-        }
-        await client.post("/users", json=user)
-        
-        auth_info = {
-            "username": "test",
-            "password": "password"
-        }
-        response = await client.post("/token", data=auth_info)
-        response_data = response.json()
-        assert response.status_code == 200
-        assert "access_token" in response_data
-        assert "refresh_token" in response_data
-        assert response_data["token_type"] == "bearer"
-    
-    @pytest.mark.anyio
-    async def test_token_unauthorized(self, client, db_setup):
-        user = {
-            "username": "test",
-            "password": "password",
-            "email": "email"
-        }
-        await client.post("/users", json=user)
-        
-        auth_info = {
-            "username": "test",
-            "password": "wrong_password"
-        }
-        response = await client.post("/token", data=auth_info)
-        assert response.status_code == 401
-    
-    @pytest.mark.anyio
     async def test_get_user_info(self, client, db_setup):
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.post("/token", data=user)
@@ -79,14 +43,14 @@ class TestEndpoints:
         response_data = response.json()
         assert response.status_code == 200
         assert response_data["username"] == "test"
-        assert response_data["email"] == "email"
+        assert response_data["email"] == "email@gmail.com"
     
     @pytest.mark.anyio
     async def test_get_user_info_unauthorized(self, client, db_setup):
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.get("/users/me")
@@ -97,7 +61,7 @@ class TestEndpoints:
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.post("/token", data=user)
@@ -106,20 +70,20 @@ class TestEndpoints:
         headers = {'Authorization': f'Bearer {token}'}
         update_info = {
             "username": "test2",
-            "email": "email2"
+            "email": "email2@gmail.com"
         }
         response = await client.put("/users", json=update_info, headers=headers)
         response_data = response.json()
         assert response.status_code == 200
         assert response_data["username"] == "test2"
-        assert response_data["email"] == "email2"
+        assert response_data["email"] == "email2@gmail.com"
         
     @pytest.mark.anyio
     async def test_update_user_password(self, client, db_setup):
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.post("/token", data=user)
@@ -138,13 +102,14 @@ class TestEndpoints:
     async def test_update_user_duplicate_username(self, client, db_setup):
         other_user = {
             "username": "test2",
-            "password": "password2"
+            "password": "password2",
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=other_user)
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.post("/token", data=user)
@@ -162,13 +127,13 @@ class TestEndpoints:
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         
         update_info = {
             "username": "test2",
-            "email": "email2"
+            "email": "email2@gmail.com"
         }
         response = await client.put("/users", json=update_info)
         assert response.status_code == 401
@@ -178,7 +143,7 @@ class TestEndpoints:
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.post("/token", data=user)
@@ -198,8 +163,98 @@ class TestEndpoints:
         user = {
             "username": "test",
             "password": "password",
-            "email": "email"
+            "email": "email@gmail.com"
         }
         await client.post("/users", json=user)
         response = await client.delete("/users")
+        assert response.status_code == 401
+        
+class TestAuthEndpoints:
+    @pytest.mark.anyio
+    async def test_token(self, client, db_setup):
+        user = {
+            "username": "test",
+            "password": "password",
+            "email": "email@gmail.com"
+        }
+        await client.post("/users", json=user)
+        
+        auth_info = {
+            "username": "test",
+            "password": "password"
+        }
+        response = await client.post("/token", data=auth_info)
+        response_data = response.json()
+        assert response.status_code == 200
+        assert "access_token" in response_data
+        assert "expires_in" in response_data
+        assert response_data["token_type"] == "Bearer"
+    
+    @pytest.mark.anyio
+    async def test_token_unauthorized(self, client, db_setup):
+        auth_info = {
+            "username": "test",
+            "password": "wrong_password"
+        }
+        response = await client.post("/token", data=auth_info)
+        assert response.status_code == 401
+    
+    @pytest.mark.anyio
+    async def test_refresh_token(self, client, db_setup):
+        user = {
+            "username": "test",
+            "password": "password",
+            "email": "email@gmail.com"
+        }
+        await client.post("/users", json=user)
+        response = await client.post("/token", data=user)
+        token = response.json()["access_token"]
+        
+        headers = {'Authorization': f'Bearer {token}'}
+        response = await client.get("/refresh_token", headers=headers)
+        response_data = response.json()
+        assert response.status_code == 200
+        assert "refresh_token" in response_data
+        assert "expires_in" in response_data
+
+    @pytest.mark.anyio
+    async def test_refresh_token_unauthorized(self, client, db_setup):
+        response = await client.get("/refresh_token")
+        assert response.status_code == 401
+    
+    @pytest.mark.anyio
+    async def test_refresh(self, client, db_setup):
+        user = {
+            "username": "test",
+            "password": "password",
+            "email": "email@gmail.com"
+        }
+        await client.post("/users", json=user)
+        response = await client.post("/token", data=user)
+        token = response.json()["access_token"]
+        
+        headers = {'Authorization': f'Bearer {token}'}
+        response = await client.get("/refresh_token", headers=headers)
+        refresh_token = response.json()["refresh_token"]
+        
+        response = await client.post("/refresh", json= {"refresh_token": refresh_token}, headers=headers)
+        response_data = response.json()
+        assert response.status_code == 200
+        assert "access_token" in response_data
+        assert "expires_in" in response_data
+        assert response_data["token_type"] == "Bearer"
+    
+    @pytest.mark.anyio
+    async def test_refresh_unauthorized(self, client, db_setup):
+        user = {
+            "username": "test",
+            "password": "password",
+            "email": "email@gmail.com"
+        }
+        await client.post("/users", json=user)
+        response = await client.post("/token", data=user)
+        token = response.json()["access_token"]
+        
+        headers = {'Authorization': f'Bearer {token}'}        
+        response = await client.post("/refresh", json= {"refresh_token": "invalid_token"}, headers=headers)
         assert response.status_code == 401
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,11 +2,14 @@ from datetime import timedelta, datetime, timezone
 from unittest.mock import patch
 
 import pytest
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
 
 from env_setup import db_setup, db_session, anyio_backend
 from utils.password_utils import verify_password, get_password_hash
-from utils.jwt import create_access_token, create_refresh_token, decode_token
+from utils.jwt import create_access_token, create_refresh_token, decode_token, get_jwt_username
 from utils.crud import UserCRUD, TokenCRUD
+import utils.crud
 from schemas.user_schemas import UserCreate, UserUpdate
 from schemas.token_schemas import TokenCreate
 
@@ -47,68 +50,139 @@ class TestJWT:
             expected_expire_timestamp = (now + timedelta(seconds=10)).timestamp()
             assert payload["sub"] == "test"
             assert abs(expected_expire_timestamp - payload["exp"]) < 1
+    
+    def test_get_jwt_username(self):
+        access_token = create_access_token("test", timedelta(seconds=10))
+        username = get_jwt_username(access_token)
+        assert username == "test"
+    
+    def test_get_jwt_username_invalid_jwt(self):
+        with pytest.raises(HTTPException) as http_401:
+            get_jwt_username("invalid_token")
+        assert http_401.value.status_code == 401
 
 class TestCRUD:    
     @pytest.mark.anyio
     async def test_create_user(self, db_setup, db_session):
-        user = UserCreate(username="test", password="password", email="email")
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         user = await UserCRUD.create_user(db_session, user)
         assert user.username == "test"
         assert verify_password("password", user.password_hash) == True
-        assert user.email == "email"
+        assert user.email == "email@gmail.com"
     
     @pytest.mark.anyio
     async def test_get_user_by_username(self, db_setup, db_session):
-        user = UserCreate(username="test", password="password", email="email")
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         await UserCRUD.create_user(db_session, user)
         user = await UserCRUD.get_user_by_username(db_session, "test")
         assert user.username == "test"
         assert verify_password("password", user.password_hash) == True
-        assert user.email == "email"    
+        assert user.email == "email@gmail.com"    
     
     @pytest.mark.anyio
     async def test_get_user_by_id(self, db_setup, db_session):
-        user = UserCreate(username="test", password="password", email="email")
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         user = await UserCRUD.create_user(db_session, user)
         user = await UserCRUD.get_user_by_id(db_session, user.id)
         assert user.username == "test"
         assert verify_password("password", user.password_hash) == True
-        assert user.email == "email"
+        assert user.email == "email@gmail.com"
     
     @pytest.mark.anyio
     async def test_delete_user_by_username(self, db_setup, db_session):
-        user = UserCreate(username="test", password="password", email="email")
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         user = await UserCRUD.create_user(db_session, user)
         delete_count = await UserCRUD.delete_user_by_username(db_session, "test")
         assert delete_count == 1
     
     @pytest.mark.anyio
     async def test_update_user(self, db_setup, db_session):
-        update_info = UserUpdate(username="test2", password="password2", email="email2")
-        user = UserCreate(username="test", password="password", email="email")
+        update_info = UserUpdate(username="test2", password="password2", email="email2@gmail.com")
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         user = await UserCRUD.create_user(db_session, user)
         user = await UserCRUD.update_user(db_session, user.id, update_info)
         assert user.username == "test2"
         assert verify_password("password2", user.password_hash) == True
-        assert user.email == "email2"
+        assert user.email == "email2@gmail.com"
 
     @pytest.mark.anyio
-    async def test_create_token(self, db_setup, db_session):
-        user = UserCreate(username="test", password="password", email="email")
+    async def test_create_access_token(self, db_setup, db_session):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         user = await UserCRUD.create_user(db_session, user)
         user_id = user.id
-        token = TokenCreate(access_token="access token", refresh_token="refresh token", user_id=user_id)
-        token = await TokenCRUD.create_token(db_session, token)
+        token = TokenCreate(token="access token", user_id=user_id)
+        token = await TokenCRUD.create_access_token(db_session, token)
         assert token.access_token == "access token"
-        assert token.refresh_token == "refresh token"
-        assert token.status == True
         assert token.user_id == user_id
     
     @pytest.mark.anyio
-    async def test_delete_expired_tokens(self, db_setup, db_session):
-        user = UserCreate(username="test", password="password", email="email")
+    async def test_create_refresh_token(self, db_setup, db_session):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
         user = await UserCRUD.create_user(db_session, user)
-        token = TokenCreate(access_token="access token", refresh_token="refresh token", user_id=user.id)
-        token = await TokenCRUD.create_token(db_session, token)
-        delete_count = await TokenCRUD.delete_expired_tokens(db_session, timedelta(seconds=0))
+        user_id = user.id
+        token = TokenCreate(token="refresh token", user_id=user_id)
+        token = await TokenCRUD.create_refresh_token(db_session, token)
+        assert token.refresh_token == "refresh token"
+        assert token.user_id == user_id
+    
+    @pytest.mark.anyio
+    async def test_create_refresh_token_duplicate_userid(self, db_setup, db_session):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
+        user = await UserCRUD.create_user(db_session, user)
+        user_id = user.id
+        token = TokenCreate(token="refresh token", user_id=user_id)
+        await TokenCRUD.create_refresh_token(db_session, token)
+        with pytest.raises(IntegrityError) as duplicate_userid:
+            await TokenCRUD.create_refresh_token(db_session, token)
+        assert duplicate_userid.typename == "IntegrityError"
+        
+    @pytest.mark.anyio
+    async def test_get_refresh_token_by_userid(self, db_setup, db_session):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
+        user = await UserCRUD.create_user(db_session, user)
+        user_id = user.id
+        token = TokenCreate(token="refresh token", user_id=user_id)
+        await TokenCRUD.create_refresh_token(db_session, token)
+        token = await TokenCRUD.get_refresh_token_by_userid(db_session, user_id)
+        assert token.refresh_token == "refresh token"
+        assert token.user_id == user_id
+    
+    @pytest.mark.anyio
+    async def test_update_refresh_token(self, db_setup, db_session):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
+        user = await UserCRUD.create_user(db_session, user)
+        user_id = user.id
+        token = TokenCreate(token="refresh token", user_id=user_id)
+        await TokenCRUD.create_refresh_token(db_session, token)
+        token = TokenCreate(token="refresh token 2", user_id=user_id)
+        token = await TokenCRUD.update_refresh_token(db_session, token)
+        assert token.refresh_token == "refresh token 2"
+        assert token.user_id == user_id
+    
+    @pytest.fixture
+    def override_token_expiration(self):
+        original_access_expire_value = utils.crud.ACCESS_TOKEN_EXPIRE_MINUTES
+        original_refresh_expire_value = utils.crud.REFRESH_TOKEN_EXPIRE_DAYS
+        utils.crud.ACCESS_TOKEN_EXPIRE_MINUTES = 0
+        utils.crud.REFRESH_TOKEN_EXPIRE_DAYS = 0
+        yield
+        utils.crud.ACCESS_TOKEN_EXPIRE_MINUTES = original_access_expire_value
+        utils.crud.REFRESH_TOKEN_EXPIRE_DAYS = original_refresh_expire_value
+
+    @pytest.mark.anyio
+    async def test_delete_expired_access_tokens(self, db_setup, db_session, override_token_expiration):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
+        user = await UserCRUD.create_user(db_session, user)
+        token = TokenCreate(token="access token",user_id=user.id)
+        token = await TokenCRUD.create_access_token(db_session, token)
+        delete_count = await TokenCRUD.delete_expired_access_tokens(db_session)
+        assert delete_count == 1
+    
+    @pytest.mark.anyio
+    async def test_delete_expired_refresh_tokens(self, db_setup, db_session, override_token_expiration):
+        user = UserCreate(username="test", password="password", email="email@gmail.com")
+        user = await UserCRUD.create_user(db_session, user)
+        token = TokenCreate(token="refresh token",user_id=user.id)
+        token = await TokenCRUD.create_refresh_token(db_session, token)
+        delete_count = await TokenCRUD.delete_expired_refresh_tokens(db_session)
         assert delete_count == 1
